257da2f7cc6576031a94ae9d16138fbc467c6052
Gary Bernhardt <gary.bernhardt@gmail.com>
2014-03-10 11:40:27 -0700

    hacked up recursive algorthm

--- spec/score_spec.rb
+++ spec/score_spec.rb
@@ -6,39 +6,39 @@ describe "score" do
   end
 
   describe "basic matching" do
-    it "scores 0 when the choice is empty" do
-      score("", "a").should == 0
+    it "scores infinity when the choice is empty" do
+      score("", "a").should == Float::INFINITY
     end
 
-    it "scores 1 when the query is empty" do
-      score("a", "").should == 1
+    it "scores 0 when the query is empty" do
+      score("a", "").should == 0.0
     end
 
-    it "scores 0 when the query is longer than the choice" do
-      score("short", "longer").should == 0
+    it "scores infinity when the query is longer than the choice" do
+      score("short", "longer").should == Float::INFINITY
     end
 
-    it "scores 0 when the query doesn't match at all" do
-      score("a", "b").should == 0
+    it "scores infinity when the query doesn't match at all" do
+      score("a", "b").should == Float::INFINITY
     end
 
-    it "scores 0 when only a prefix of the query matches" do
-      score("ab", "ac").should == 0
+    it "scores infinity when only a prefix of the query matches" do
+      score("ab", "ac").should == Float::INFINITY
     end
 
-    it "scores greater than 0 when it matches" do
-      score("a", "a").should be > 0
-      score("ab", "a").should be > 0
-      score("ba", "a").should be > 0
-      score("bab", "a").should be > 0
-      score("babababab", "aaaa").should be > 0
+    it "scores less than infinity when it matches" do
+      score("a", "a").should be < Float::INFINITY
+      score("ab", "a").should be < Float::INFINITY
+      score("ba", "a").should be < Float::INFINITY
+      score("bab", "a").should be < Float::INFINITY
+      score("babababab", "aaaa").should be < Float::INFINITY
     end
 
-    it "scores 1 when the choice contains the exact substring" do
-      score("a", "a").should == 1.0
-      score("ab", "ab").should == 1.0
-      score("a long string", "a long string").should == 1.0
-      score("spec/search_spec.rb", "sear").should == 1.0
+    it "scores the length of the query when the query is a substring" do
+      score("xa", "a").should == "a".length
+      score("xab", "ab").should == "ab".length
+      score("xalongstring", "alongstring").should == "alongstring".length
+      score("lib/search.rb", "earc").should == "earc".length
     end
   end
 
@@ -53,29 +53,29 @@ describe "score" do
     end
 
     it "doesn't match when the same letter is repeated in the choice" do
-      score("a", "aa").should == 0.0
+      score("a", "aa").should == Float::INFINITY
     end
   end
 
   describe "match quality" do
     it "scores higher for better matches" do
-      score("selecta.gemspec", "asp").should be > score("algorithm4_spec.rb", "asp")
-      score("README.md", "em").should be > score("benchmark.rb", "em")
+      score("selecta.gemspec", "asp").should be < score("algorithm4_spec.rb", "asp")
+      score("README.md", "em").should be < score("benchmark.rb", "em")
     end
 
     it "sometimes scores longer strings higher if they have a better match" do
-      score("long 12 long", "12").should be > score("1 long 2", "12")
+      score("long12long", "12").should be < score("1long2", "12")
     end
 
     it "scores the tighter of two matches, regardless of order" do
       tight = "12"
       loose = "1padding2"
-      score(tight + loose, "12").should == 1.0
-      score(loose + tight, "12").should == 1.0
+      score(tight + loose, "12").should == 2
+      score(loose + tight, "12").should == 2
     end
-  end
 
-  xit "prefers acronyms to normal matches" do
-    score("Foo Bar", "fb").should be > score("foo bar", "fb")
+    it "scores characters at word boundaries higher" do
+      score("foo/bar", "ba").should be < score("foobar", "ba")
+    end
   end
 end
